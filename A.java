8f8b5260dd565bcf0538cb79409278ec563e43f2
ambercs18 <53806443+ambercs18@users.noreply.github.com>
2019-08-30 19:15:33 +0530

    Update A.java

--- A.java
+++ A.java
@@ -7,32 +7,47 @@ Scanner x=new Scanner(System.in);
 System.out.println("Enter no. of students and no. of subjects");
 int n=x.nextInt();
 int m=x.nextInt();
-int m[]=new int[n+2][m+2];
+float mr[][]=new float[n+2][m+2];
 System.out.println("Enter Marks");
 for(int i=0;i<n;i++)
 {
 for(int j=0;j<n;j++)
 {
-m[i][j]=x.nextInt();
+mr[i][j]=x.nextFloat();
 }
 }
 for(int i=0;i<n;i++)
 {
-int s,a=0;
+float s=0;
+  float a=0;
 for(int j=0;j<m;j++)
 {
-s=s+m[i][j];
+s=s+mr[i][j];
 a=s/m;
-m[0][n]=s
-m[0][n+1]=a;
 }
+  mr[i][n]=s;
+  mr[i][n+1]=a;
 }
 for(int i=0;i<n;i++)
 {
-int s,a=0;
+float s=0;
+  float a=0;
 for(int j=0;j<m;j++)
 {
-s=s+m[j][i];
+s=s+mr[j][i];
 a=s/m;
-m[n][0]=s;
-m[n+1][0]=a;
+}
+  mr[n][i]=s;
+  mr[n+1][i]=a;
+}
+  for (float a[]:mr)
+  {
+    for (float y:a)
+    { 
+    	if(y!=0)
+      System.out.print(y+"\t");
+    }
+    System.out.println();
+  }
+}
+}
