fdf4070861954adc6178de54c41d1108d6ed5723
ambercs18 <53806443+ambercs18@users.noreply.github.com>
2019-09-06 15:37:38 +0530

    Add files via upload

---
+++ Semi.java
@@ -0,0 +1,71 @@
+import java.util.*;
+public class Semi
+{
+    public static void main(String[] args) {
+		int n;
+int f=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+n=sc.nextInt();
+for(int i=2;i<=n/2;i++)
+{
+if(checks(i)==1 && checks(n-i)==1)
+{
+f=1;
+break;
+}
+else
+continue;
+}
+if(f==1)
+System.out.println("yesss");
+else
+System.out.println("nnnnnn");
+}
+static int checks(int m)
+{
+int ar[]=new int[100];
+int c=0;
+int f=0;
+int x=1;
+    for(int i=2;i<=m/2;i++)
+    {
+        if(m%i==0)
+        {
+            ar[c]=i;
+            c++;
+        }
+    }
+    for(int i=0;i<ar.length;i++)
+    {
+        if(prime(ar[i])==1)
+        {
+             x*=ar[i];
+            if(x==m)
+            {
+            f=1;
+            break;
+            }
+            
+        }
+    }
+    if(f==1)
+    return 1;
+    else
+    return 0;
+}
+static int prime(int n)
+{
+int c=0;
+for(int i=2;i<=n/2;i++)
+{
+    if(n%i==0)
+    c++;
+}
+if(c==0)
+return 1;
+else
+return 0;
+    
+}
+}
